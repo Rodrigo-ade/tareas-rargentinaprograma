405f4107b4b79412d097237348310cc1243df9e0
Rodrigo <zoccorodrigo@gmail.com>
2023-01-13 15:30:49 -0300

    Agrega la tarea parte 2

--- clase-6/Clase-6-parte-2/clase6-parte-2.js
+++ clase-6/Clase-6-parte-2/clase6-parte-2.js
@@ -1,7 +1,111 @@
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+function ocultarElemento(elemento){
+    elemento.style.visibility = "hidden";
+}
 
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
+function desocultarElemento(elemento){
+    elemento.style.visibility = "visible";
+}
+
+let $botonAgregar = document.querySelector("#agregar");
+let $botonQuitar = document.querySelector("#quitar");
+let $botonCalcular = document.querySelector("#calcular");
+
+$botonAgregar.onclick = function(){
+    agregarIntegrante();
+    desocultarElemento($botonCalcular);
+
+}
+
+$botonQuitar.onclick = function(){
+    eliminarIntegrante();
+    if(numeroIntegrante === 0){
+        ocultarElemento($botonCalcular);
+    }
+}
+
+let numeroIntegrante = 0;
+function agregarIntegrante(){
+    let etiquetaIntegrante = document.createElement("label");
+    let textoEtiquetaIntegrante = document.createTextNode(`Ingresa el salario anual del ${numeroIntegrante + 1}º integrante: `);
+    etiquetaIntegrante.appendChild(textoEtiquetaIntegrante);
+
+    let inputIntegrante = document.createElement("input");
+    inputIntegrante.type = "number";
+    inputIntegrante.className = "salario-integrante";
+
+    let divIntegrante = document.createElement("div");
+    divIntegrante.appendChild(etiquetaIntegrante);
+    divIntegrante.appendChild(inputIntegrante);
+    divIntegrante.id = `integrante-${numeroIntegrante}`;
+
+    document.querySelector("#lista-familiares").appendChild(divIntegrante);
+    numeroIntegrante ++;
+}
+
+function eliminarIntegrante(){
+    if(numeroIntegrante === 0){
+        alert("Para eliminar un integrante primero debes crearlo");
+    }else{
+        document.querySelector(`#integrante-${numeroIntegrante-1}`).remove();
+        numeroIntegrante --;
+    }
+}
+
+$botonCalcular.onclick = function(){
+    desocultarElemento(document.querySelector("#lista-resultados"));
+    let salariosIntegrantes = obtenerSalariosIntegrantes();
+    agregarResultadoSalario("mayor", obtenerSalarioMayor(salariosIntegrantes));
+    agregarResultadoSalario("menor", obtenerSalarioMenor(salariosIntegrantes));
+    agregarResultadoSalario("promedio-anual", obtenerPromedioAnual(salariosIntegrantes));
+    agregarResultadoSalario("promedio-mensual", obtenerPromedioMensual(salariosIntegrantes));
+}
+
+function obtenerSalariosIntegrantes(){
+    let listaSalarios = document.querySelectorAll(".salario-integrante");
+    let salariosIntegrantes = [];
+    for(let i= 0; i<listaSalarios.length; i++){
+        salariosIntegrantes.push(Number(listaSalarios[i].value));
+    }
+    return salariosIntegrantes;
+}
+
+function agregarResultadoSalario(idTipo, valor){
+    document.querySelector(`#${idTipo}-salario`).textContent = valor;
+}
+
+function obtenerSalarioMayor(arraySalarios){
+    let salarioMayor = -Infinity;
+    for(let i = 0; i<arraySalarios.length; i++){
+        if(salarioMayor < arraySalarios[i]){
+            salarioMayor = arraySalarios[i];
+        }
+    }
+    return salarioMayor;
+}
+
+function obtenerSalarioMenor(arraySalarios){
+    let salarioMenor = +Infinity;
+    for(let i=0; i<arraySalarios.length; i++){
+        if(salarioMenor > arraySalarios[i]){
+            salarioMenor = arraySalarios[i];
+        }
+    }
+    return salarioMenor;
+}
+
+function obtenerPromedioAnual(arraySalarios){
+    let sumaSalarios = 0;
+    for(let i = 0; i<arraySalarios.length; i++){
+        sumaSalarios += arraySalarios[i];
+    }
+    return sumaSalarios / arraySalarios.length;
+}
+
+function obtenerPromedioMensual(arraySalarios){
+    let sumaSalarios = 0;
+    const MESES_POR_ANIO = 12;
+    for(let i = 0; i<arraySalarios.length; i++){
+        sumaSalarios += arraySalarios[i];
+    }
+    return sumaSalarios / (MESES_POR_ANIO * arraySalarios.length);
+}
