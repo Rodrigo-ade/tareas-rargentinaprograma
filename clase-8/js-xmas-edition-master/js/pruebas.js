4f979059d827aa5c03c365956da18e3a2386a93b
Rodrigo <zoccorodrigo@gmail.com>
2023-01-17 15:37:49 -0300

    Añade validaciones con regex y pruebas
    Porque era lo pedido en la tarea 8

---
+++ clase-8/js-xmas-edition-master/js/pruebas.js
@@ -0,0 +1,59 @@
+function probarValidarNombre(){
+    console.assert(
+        validarNombre("") === 'El nombre debe tener al menos 1 caracter',
+        "validarNombre no validó que el nombre no esté vacio",
+    );
+
+    console.assert(
+        validarNombre("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") === 'El nombre debe tener menos de 20 caracteres',
+        "validarNombre no validó que el nombre tenga menos de 20 caracteres.",
+    );
+
+    console.assert(
+        validarNombre("RiCaRdO") === "El nombre debe comenzar con mayuscula y contener solo letras minusculas",
+        "validarNombre no validó que el nombre comienze con mayuscula y tenga sólo minsuculas",
+    );
+
+    console.assert(
+        validarNombre("Rodrigo") === "",
+        "validarNombre no funcionó con un nombre correcto",
+    );
+}
+probarValidarNombre();
+
+function probarValidarCiudad(){
+    console.assert(
+        validarCiudad("") === "Este campo debe tener al menos 1 caracter",
+        "validarCiudad no validó que ciudad no sea vacio",
+    );
+
+    console.assert(
+        validarCiudad("Buenos Aires") === "",
+        "validarCiudad no funcionó con una ciudad Correcta",
+    );
+
+}
+probarValidarCiudad();
+
+function probarValidarDescripcionRegalo(){
+    console.assert(
+        validarDescripcionRegalo("") === "Este campo debe tener al menos 1 caracter",
+        "validarDescripcionRegalo no validó que descripcionRegalo no sea vacio",
+    );
+
+    console.assert(
+        validarDescripcionRegalo("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") === "Este campo debe tener menos de 100 caracteres",
+        "validarDescripcionRegalo no validó que descripcionRegalo tenga menos de 100 caracteres",
+    );
+
+    console.assert(
+        validarDescripcionRegalo("Quiero una pelot@%&") === `Este campo solo permite letras, numeros, espacios, "," y "." `,
+        "validarDescripcionRegalo no validó que descripcionRegalo tenga solo letras, numeros, espacios, comas y puntos",
+    );
+
+    console.assert(
+        validarDescripcionRegalo("Una computadora") === "",
+        "validarDescripcionRegalo no funcionó con una descripcionRegalo correcta",
+    );
+}
+probarValidarDescripcionRegalo();
