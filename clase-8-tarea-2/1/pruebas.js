f28c56ff274ca2542a79ae5630503ec6516ad1ab
Rodrigo <zoccorodrigo@gmail.com>
2023-01-19 08:12:39 -0300

    Agrega validaciones y pruebas a la 1º tarea

--- clase-8-tarea-2/1/pruebas.js
+++ clase-8-tarea-2/1/pruebas.js
@@ -1,5 +1,10 @@
 function probarValidarCantidadFamiliares(){
     console.assert(
+        validarCantidadFamiliares(123) === "La cantidad de familiares solo acepta numeros de 2 cifras",
+        "validarCantidadFamiliares no valido que cantidadFamiliares sea un numero de 2 cifras",
+    );
+    
+    console.assert(
         validarCantidadFamiliares(0) === "Debes ingresar como mínimo 1 familiar",
         "validarCantidadFamiliares no valido que cantidadFamiliares sea mayor a 0",
     );
@@ -18,13 +23,18 @@ probarValidarCantidadFamiliares();
 
 function probarValidarEdadFamiliar(){
     console.assert(
+        validarEdadFamiliar(1000) === "La edad de tu familiar debe ser un número entre 1 y 3 cifras",
+        "validarEdadFamiliar no valido que edadFamiliar sea un número entre 1 y 3 cifras",
+    );
+
+    console.assert(
         validarEdadFamiliar(0) === "Los familiares deben tener más de 0 años",
         "validarEdadFamiliar no valido que edadFamiliar sea menor a 0",
     );
 
     console.assert(
-        validarEdadFamiliar(111) === "Un familiar no puede tener más de 110 años",
-        "validarEdadFamiliar no valido que edadFamiliar sea menor a 110",
+        validarEdadFamiliar(120) === "Un familiar no puede tener más de 120 años",
+        "validarEdadFamiliar no valido que edadFamiliar sea menor a 120",
     );
 
     console.assert(
--- clase-8-tarea-2/1/validaciones.js
+++ clase-8-tarea-2/1/validaciones.js
@@ -1,12 +1,12 @@
-/*
-cantidad de familiares mayor a 0 y menor a 10.
-edad de familiares mayor a 0 y menor a 110.
-*/
-
 function validarCantidadFamiliares(cantidadFamiliares){
+    if(!/^[0-9]{1,2}$/.test(cantidadFamiliares)){
+        return "La cantidad de familiares solo acepta numeros de 2 cifras";
+    }
+
     if(cantidadFamiliares <= 0){
         return "Debes ingresar como mínimo 1 familiar";
     }
+    
     if(cantidadFamiliares >= 10){
         return "Como máximo puedes agregar 10 familiares";
     }
@@ -15,11 +15,16 @@ function validarCantidadFamiliares(cantidadFamiliares){
 }
 
 function validarEdadFamiliar(edadFamiliar){
+    if(!/^[0-9]{1,3}$/.test(edadFamiliar)){
+        return "La edad de tu familiar debe ser un número entre 1 y 3 cifras";
+    }
+
     if(edadFamiliar <= 0){
         return "Los familiares deben tener más de 0 años";
     }
-    if(edadFamiliar > 110){
-        return "Un familiar no puede tener más de 110 años";
+
+    if(edadFamiliar >= 120){
+        return "Un familiar no puede tener más de 120 años";
     }
     
     return "";
