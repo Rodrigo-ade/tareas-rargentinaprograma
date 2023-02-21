c0ee3d911ef79b70af9783ca15f24043988465c3
Rodrigo <zoccorodrigo@gmail.com>
2023-02-21 13:16:57 -0300

    Agrega pruebas a la tarea 1

--- cypress/e2e/tarea-1-prueba.cy.js
+++ cypress/e2e/tarea-1-prueba.cy.js
@@ -1,23 +1,57 @@
-const URL = algo;
+const URL = "http://127.0.0.1:8080/clase-8-tarea-2/1/";
+const MAXIMOS_FAMILIARES = 10;
+const FAMILIARES_A_CREAR = 4;
+const EDADES_FAMILIARES_CORRECTAS = [7,18,48,53];     
+const EDADES_FAMILIARES_INCORRECTAS = [0,120,300,-53]; 
+
 
 context("Prueba tarea 1", () => {
-  describe("Pruebas completando el formulario incorrectamente", () => {
-    /*-Si agrego 11 familiares > "#errores-cantidad-familiares" 
-    NO tiene que tener class OCULTO Y
-    su <p> should.content"Como máximo puedes agregar 10 familiares"*/
+  before(() => {
+    cy.visit(URL);
+  });
 
-    /*Prueba errores:
-    -agrego 4 familiares se deben crear 4 ".divisor-familiar"
-    -Si un familiar tiene 120 o más años, ese divisor tiene q tener error 
-    -Si un familiar tiene 0 o menos años, ese divisor tiene q tener error 
-    *Ambos deben tener el "#resultados-edades" con class".oculto";*/ 
+  describe("Pruebas completando el formulario incorrectamente", () => {
+    it("Valida que no se puedan agregar más de 10 familiares", () => {
+      cy.get("#cantidad-familiares").type(MAXIMOS_FAMILIARES);
+      cy.get("#boton-cantidad-familiares").click()
+      cy.get("#errores-cantidad-familiares").should("be.visible");
+      cy.get("#errores-cantidad-familiares p").should("contain", "Como máximo puedes agregar 10 familiares");
+    });
+
+    it("Verifica que se marque el error en las casillas con edades incorrectas", () => {
+      cy.get("#empezar-nuevamente").click()
+      cy.get("#cantidad-familiares").type(FAMILIARES_A_CREAR);
+      cy.get("#boton-cantidad-familiares").click()
+
+      cy.get(".edad-familiar").then(($edadesFamiliares) => {
+        $edadesFamiliares.each((indice,$edadFamiliar) => {
+         let edad = EDADES_FAMILIARES_INCORRECTAS[indice]; 
+          cy.get($edadFamiliar).type(edad);
+        })
+      });
+
+      cy.get("#boton-calcular").click();
+
+      cy.get(".edad-familiar").then(($edadesFamiliares) => {
+        $edadesFamiliares.each((indice, $edadFamiliar)=>{
+          cy.get($edadFamiliar).should("have.id", "error");
+        });
+      });
+    });
+
+    it("Verifica que si hay edades incorrectas no se muestren resultados", () => {
+      cy.get("#resultados-edades").should("have.class", "oculto");
+    });
 
   });
 
   describe("Pruebas recomenzando el formulario", () => {
-    /*Prueba recomenzar:
-    creo 4 familiares (tiene q tener 4 familiares)
-    si clickeo recomenzar, tienen q haber 0 familiares.*/
+    it("Verifica que el boton recomenzar funcione correctamente", () => {
+      cy.get("#empezar-nuevamente").click();
+      cy.get("#edades-familiares").children().should("have.length", 0);
+    });
+    
+
   });
 
   describe("Pruebas completando el formulario correctamente", () => {
